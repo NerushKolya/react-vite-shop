11f3346bda77a5a6980fadf8d2e051b39ed70638
NerushKolya <kolyanerush@gmail.com>
2025-06-19 16:42:01 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 
 function App() {
 	return (
 		<>
 			<Button onClick={()=>console.log(123)}>Button</Button>
+			<Button appearance='big'>Button</Button>
+			<Input placeholder='Email' />
 		</>
 	);
 }
--- src/components/Button.props.ts
+++
@@ -1,6 +0,0 @@
-import type { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from 'react';
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    children: ReactNode;
-    onClick?: MouseEventHandler<HTMLButtonElement>;
-}
